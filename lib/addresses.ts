311b979d6a27d89e7085e9a95276b50223553575
okamotoyouto <gdfrh456876@outlook.com>
2022-04-15 00:14:13 +0900

    sol pay2

---
+++ lib/addresses.ts
@@ -0,0 +1,6 @@
+import { PublicKey } from "@solana/web3.js"
+
+// Your shop wallet address
+export const shopAddress = new PublicKey('8ev2fJkwhZpnRFpk83kFnrdWRRdLmdFzmcFroo7Fvf1k') 
+// this is the same for everyone!
+export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
